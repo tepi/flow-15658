126c7d898c2984c2ef1529380afe24abc3666a79
Teppo Kurki <teppo.kurki@vaadin.com>
2023-01-20 09:06:23 +0200

    Add images and PWA

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -2,6 +2,7 @@ package com.example.application;
 
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,6 +18,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Theme(value = "myapp")
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 @NpmPackage(value = "@vaadin-component-factory/vcf-nav", version = "1.0.6")
+@PWA(name = "ImageTest", shortName = "ImageTest")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
--- src/main/java/com/example/application/views/helloworld/HelloWorldView.java
+++ src/main/java/com/example/application/views/helloworld/HelloWorldView.java
@@ -1,14 +1,21 @@
 package com.example.application.views.helloworld;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import com.example.application.views.MainLayout;
+
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.server.StreamResource;
 
 @PageTitle("Hello World")
 @Route(value = "hello", layout = MainLayout.class)
@@ -19,17 +26,21 @@ public class HelloWorldView extends HorizontalLayout {
     private Button sayHello;
 
     public HelloWorldView() {
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
-        sayHello.addClickShortcut(Key.ENTER);
-
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
-
-        add(name, sayHello);
+        for (int i = 1; i <= 5; i++)
+            add(getImage("sample" + i + ".jpg"));
     }
 
+
+    public Image getImage(String resourceName) {
+        InputStream ioStream = this.getClass().getClassLoader().getResourceAsStream(resourceName);
+        try {
+            byte[] buf = ioStream.readAllBytes();
+            Image img = new Image(new StreamResource("", () -> new ByteArrayInputStream(buf)), "");
+            img.setWidth("200px");
+            img.setHeight("200px");
+            return img;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
